a1e0c687ab94e7f285a0e61b1ab9326fbc4bdd14
palimpZest <polo.vaca365@gmail.com>
2018-02-18 20:58:43 +0100

    feat: Add test api route

--- back/routes/index.js
+++ back/routes/index.js
@@ -7,4 +7,121 @@ router
   .get(Calls.read)
   .post(Calls.create);
 
+router
+  .route("/api")
+  .get(function(req, res, next) {
+    res.send({floors});
+  });
+
 module.exports = router;
+
+const floors = {
+  count: 15,
+  results: [
+    {
+      floor_number: 9,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 8,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 7,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 6,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 5,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 4,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 3,
+      floor_position: "yes",
+      door_status: "open",
+      vertical_status: "immobile",
+      call_activation: "disabled"
+    },
+    {
+      floor_number: 2,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 1,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: 0,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: -1,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: -2,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: -3,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: -4,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    },
+    {
+      floor_number: -5,
+      floor_position: "no",
+      door_status: "closed",
+      vertical_status: "immobile",
+      call_activation: "possible"
+    }
+  ]
+};
