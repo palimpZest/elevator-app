be013be5b7311a26ce772ee103177417f15a1425
palimpZest <polo.vaca365@gmail.com>
2018-02-18 15:00:40 +0100

    feat: Add controller

--- back/controllers/Calls.js
+++ back/controllers/Calls.js
@@ -1,1 +1,30 @@
 const Call = require("../models/Call");
+
+const Calls = {
+  create: (req, res, next) => {
+    new Call({
+      name: req.body.name || req.query.name,
+      floor: req.body.floor || req.query.floor
+    }).save(err => {
+      if (err) {
+        res.render(err);
+      } else {
+        console.log(req.body);
+        console.log("Elevator called");
+        res.send("New elevator call !");
+      }
+    });
+  },
+  read: (req, res, next) => {
+    Call.find({}, "name floor").exec((error, results) => {
+      if (results.length == 0) {
+        res.send("There's been an error. No data.");
+      } else {
+        res.send(results);
+        console.log(results);
+      }
+    });
+  }
+};
+
+module.exports = Calls;
