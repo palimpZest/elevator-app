f2fdd1521ee40114db0a55a0e3049bd4d05639e9
palimpZest <polo.vaca365@gmail.com>
2018-02-19 10:14:52 +0100

    feat: Add actions and reducers

---
+++ front/src/actions/calls.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+export const ADD_CALL_SUCCESS = "ADD_CALL_SUCCESS";
+export const GET_ALL_CALLS_SUCCESS = "GET_ALL_CALLS_SUCCESS";
+export const GET_ALL_FLOORS_SUCCESS = "GET_ALL_FLOORS_SUCCESS";
+
+const API_URL = "http://localhost:3000/api";
+
+function addCallSuccess(push) {
+  return {
+    type: ADD_CALL_SUCCESS,
+    push
+  };
+}
+
+export function addCall(push) {
+  return dispatch => {
+    axios
+      .post(API_URL, { calls: push })
+      .then(function(response) {
+        console.log(response);
+        dispatch(addPostSuccess(response.data));
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+}
+
+function getAllCallsSuccess(push) {
+  return {
+    type: GET_ALL_CALLS_SUCCESS,
+    push
+  };
+}
+
+export function getCallsAsync() {
+  return dispatch => {
+    axios
+      .get(API_URL)
+      .then(function(response) {
+        console.log(response);
+        dispatch(getAllCallsSuccess(response.data));
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+}
+
+function getAllFloorsSuccess(floor) {
+  return {
+    type: GET_ALL_FLOORS_SUCCESS,
+    floor
+  };
+}
+
+export function getFloorsAsync() {
+  return dispatch => {
+    axios
+      .get(API_URL)
+      .then(function(response) {
+        console.log(response);
+        dispatch(getAllFloorsSuccess(response.data));
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+}
---
+++ front/src/reducers/calls.js
@@ -0,0 +1,25 @@
+import {
+  ADD_CALL_SUCCESS,
+  GET_ALL_CALLS_SUCCESS,
+  GET_ALL_FLOORS_SUCCESS
+} from "../actions/calls";
+
+export function calls(state = { calls: [] }, action) {
+  switch (action.type) {
+    case GET_ALL_CALLS_SUCCESS:
+      return { ...state, calls: action.push };
+    case ADD_CALL_SUCCESS:
+      return { ...state, posts: state.calls.concat(action.push) };
+    default:
+      return state;
+  }
+}
+
+export function floors(state = { floors: [] }, action) {
+  switch (action.type) {
+    case GET_ALL_FLOORS_SUCCESS:
+      return { ...state, floors: action.floor };
+    default:
+      return state;
+  }
+}
